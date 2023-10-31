6ab795e8026b34fa9e19a219a690af906510b956
fairg <fairg@LAPTOP_GIGABYTE>
2023-10-31 17:40:15 +0100

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseApp(object sender, MouseButtonEventArgs e)
+        {
+            Close();
+        }
+
+        private void MinimalizeApp(object sender, MouseButtonEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+
+        private bool isFullScreen = false;
+        private void FullScreen(object sender, MouseButtonEventArgs e)
+        {
+            if (isFullScreen)
+            {
+                WindowStyle = WindowStyle.None;
+                WindowState = WindowState.Normal;
+                ResizeMode = ResizeMode.NoResize;
+                isFullScreen = false;
+            }
+            else
+            {
+                WindowStyle = WindowStyle.None;
+                WindowState = WindowState.Maximized;
+                ResizeMode = ResizeMode.NoResize;
+                isFullScreen = true;
+            }
+        }
+    }
+}
