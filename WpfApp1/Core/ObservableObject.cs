290148e5852b5ebcd3675e04ae2e3bb16bfb28cd
Kacper Lachor <fairgames1221@gmail.com>
2023-11-28 17:16:58 +0100

    Implementacja zasady SOLID

--- WpfApp1/Core/ObservableObject.cs
+++ WpfApp1/Core/ObservableObject.cs
@@ -4,27 +4,14 @@ using System.Runtime.CompilerServices;
 
 namespace WpfApp1.Core
 {
-
-    class ObservableObject : INotifyPropertyChanged, IDisposable
+    class ObservableObject : INotifyPropertyChanged
     {
-        private bool _disposed;
-
-
         public event PropertyChangedEventHandler PropertyChanged;
 
-        public void OnPropertyChanged([CallerMemberName] string name = null)
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
         {
-            if (!_disposed)
-            {
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-            }
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
 
-
-        public void Dispose()
-        {
-
-            _disposed = true;
-        }
     }
 }
